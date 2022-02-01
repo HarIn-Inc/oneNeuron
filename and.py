5eb5808813fe3de72e2acc3e64af4d5aa44f8ce1
HarIn-Inc <59498372+HarIn-Inc@users.noreply.github.com>
2022-02-01 23:48:13 +0530

    Logging, Creating Exception and add tqdm progress bar

--- and.py
+++ and.py
@@ -2,24 +2,40 @@ from utils.model import perceptron
 from utils.all_utils import prepare_data, save_model, save_plot
 import numpy as np
 import pandas as pd
-AND = {'x1': [0, 0, 1, 1],
+import logging
+import os
+
+logging_str = '[%(asctime)s: %(levelname)s: %(module)s] %(message)s'
+log_dir = 'Logs'
+os.makedirs(log_dir, exist_ok= True)
+logging.basicConfig(filename= os.path.join(log_dir, 'Running_logs.log'),level=logging.INFO, format=logging_str, filemode='a')
+
+def main(data, ETA, Epochs, modelpath, plotname):
+       df = pd.DataFrame(data)
+       logging.info(f'This is actual dataframe: {df}')
+       X, y = prepare_data(df)
+       model = perceptron(eta=ETA, epochs=Epochs)
+       model.fit(X, y)
+
+       _ = model.totalloss()
+
+       save_model(model, filename=modelpath)
+       save_plot(df, plotname, model)
+
+if __name__ == '__main__':
+       AND = {'x1': [0, 0, 1, 1],
        'x2': [0, 1, 0, 1],
        'y': [0, 0, 0, 1]}
+       ETA = 0.3
+       Epochs = 10
+       try:
+              logging.info('>>>>>> Starting Training >>>>>>>')
+              main(data=AND, ETA = ETA, Epochs = Epochs, modelpath= 'and.model', plotname='and.png')
+              logging.info('>>>>>> Training of model is finishes >>>>>>>')
+       except Exception as e:
+              logging.exception(e)
+              raise e
 
-df = pd.DataFrame(AND)
-print(df)
-
-X, y = prepare_data(df)
-
-ETA = 0.3
-Epochs = 10
 
 
-model = perceptron(eta = ETA, epochs = Epochs)
 
-model.fit(X, y)
-
-_ = model.totalloss()
-
-save_model(model, filename='AND.model')
-save_plot(df, 'and.png', model)
--- utils/model.py
+++ utils/model.py
@@ -1,10 +1,10 @@
 import numpy as np
-
-
+import logging
+from tqdm import tqdm
 class perceptron:
   def __init__(self, eta, epochs):
     self.weights = np.random.randn(3) * 1e-4
-    print(f'Initial weights initialization:\n {self.weights}')
+    logging.info(f'Initial weights initialization:\n {self.weights}')
     self.eta = eta
     self.epochs = epochs
 
@@ -16,21 +16,21 @@ class perceptron:
     self.y = y
 
     x_with_bias = np.c_[self.X, -np.ones((len(self.X), 1))]
-    print(f'X with bias:\n {x_with_bias}')
+    logging.info(f'X with bias:\n {x_with_bias}')
 
-    for epoch in range(self.epochs):
-      print('--' * 10)
-      print(f'For Epoch:\n {epoch}')
-      print('--' * 10)
+    for epoch in tqdm(range(self.epochs), total=self.epochs, desc='Training the Model'):
+      logging.info('--' * 10)
+      logging.info(f'For Epoch:\n {epoch}')
+      logging.info('--' * 10)
 
       y_hat = self.activationFunction(x_with_bias, self.weights) #Foreward Propogation
-      print(f'Predicted value after Foreward Pass:\n {y_hat}')
+      logging.info(f'Predicted value after Foreward Pass:\n {y_hat}')
       self.error = self.y - y_hat
-      print(f'Error Value:\n {self.error}')
+      logging.info(f'Error Value:\n {self.error}')
 
       self.weights = self.weights + self.eta * np.dot(x_with_bias.T, self.error) #Backward Propogation
-      print(f'Update weight after epoch:\n {epoch}/{self.epochs} :\n {self.weights}')
-      print('##' * 10)
+      logging.info(f'Update weight after epoch:\n {epoch}/{self.epochs} :\n {self.weights}')
+      logging.info('##' * 10)
 
   def predict(self, X):
     x_with_bias = np.c_[X, -np.ones((len(X), 1))]
@@ -38,5 +38,5 @@ class perceptron:
 
   def totalloss(self):
     total_loss = np.sum(self.error)
-    print(f'Total Loss:\n {total_loss}')
+    logging.info(f'Total Loss:\n {total_loss}')
     return total_loss
